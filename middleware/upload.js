de7e84644cd965f7be7072c7120f6f3807a07741
ironoxidey <russellhein@gmail.com>
2022-01-01 11:21:32 -0500

    changed upload size

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const multer = require("multer");
-const maxSize = 2 * 1024 * 1024;
+const maxSize = 3 * 1024 * 1024;
 
 var uploadFolder = '../porchlight-uploads';
   
