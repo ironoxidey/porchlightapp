c2e852faec68ba4523b64bc4d2da2a6a8d8a030c
ironoxidey <russellhein@gmail.com>
2022-02-28 12:42:53 -0500

    going back to setting the config outside the app install directory

--- routes/api/cloudinary.js
+++ routes/api/cloudinary.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 //const controller = require("../../controller/file.controller");
 
-const config = !process.env ? require('config') : process.env; //if there's no NODE_ENV then it's 'development', otherwise it will be 'production' and it will need to look outside of the app directory because the Github action runner overwrites it every time we push to main
+const config = !process.env
+    ? require('config')
+    : require('../../porchlight-config/default.json'); //if there's no process.env then it's 'development', otherwise it will be 'production' and it will need to look outside of the app directory because the Github action runner overwrites it every time we push to main
 
 const auth = require('../../middleware/auth');
 const Artist = require('../../models/Artist');
