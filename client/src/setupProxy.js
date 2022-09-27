69d5685ae45eb0a9e67347cf15b0ce372dd32430
Rusty Hein <russellhein@gmail.com>
2022-09-26 22:33:27 -0700

    changed port back to 5000

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-	app.use(
-		createProxyMiddleware('/api', {
-			target: 'http://localhost:5001/',
-			changeOrigin: true,
-		})
-	);
+    app.use(
+        createProxyMiddleware('/api', {
+            target: 'http://localhost:5000/',
+            changeOrigin: true,
+        })
+    );
 };
--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
