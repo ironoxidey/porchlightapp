d36fe0cde132d679ffcda4a322cfae80f5c4aa3f
Rusty Hein <russellhein@gmail.com>
2024-05-28 12:43:43 -0700

    unhid mediacontent

--- client/src/components/hosts/HostReviewsEvent.js
+++ client/src/components/hosts/HostReviewsEvent.js
@@ -1266,7 +1266,7 @@ const HostReviewsEvent = ({
                 </Grid>,
             ],
         ],
-        ...(false && {
+        ...(true && {
             //I'm intentionally hiding it right now because it's broken
             mediaContent: [
                 //23
