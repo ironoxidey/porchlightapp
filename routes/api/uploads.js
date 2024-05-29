0758c8999cdc5399251eda72a19a73c5e84953f6
Rusty Hein <russellhein@gmail.com>
2024-05-29 14:55:13 -0700

    logging generateUrl again

--- routes/api/uploads.js
+++ routes/api/uploads.js
@@ -124,6 +124,8 @@ const tusServer = new Server({
         // Respect the X-Forwarded-Proto header to generate HTTPS URLs
         const proto = req.headers['x-forwarded-proto'] || req.protocol;
         const host = req.headers['x-forwarded-host'] || req.headers.host;
+        console.log('forward to proto:', proto);
+        console.log('forward to host:', host);
         return `${proto}://${host}${req.baseUrl}/${id}`;
     },
     // generateUrl(req, { proto, host, path, id }) {
